3cbd130f9c7a019374758af17a3173d8dda1760c
Mahidhar Reddy CH <57587056+mahidharreddych@users.noreply.github.com>
2024-11-12 14:05:06 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,7 +2,7 @@ from src.math_operations import add,sub
 
 def test_add():
     assert add(2,3) == 5
-    assert add(-1,-1) == 0
+    assert add(-1,-1) == -2
     assert add(3, -1) == 2
 
 def test_sub():
